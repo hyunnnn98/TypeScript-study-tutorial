5f0eb1eb7671a3f4a7ffa50b7ebd59ef1791e09b
SeungHyun Cho <c0305sh@naver.com>
2021-06-28 12:24:57 +0900

    함수 타입 - 파라미터, 반환값, 옵셔널 파라미터

---
+++ class-note/2_functions.ts
@@ -0,0 +1,24 @@
+// 함수의 파라미터에 타입을 정의하는 방식
+function sum(a: number, b: number) {
+    return a + b;
+}
+
+sum(10, 20);
+
+// 함수의 반환 값에 타입을 정의하는 방식
+function add(): number {
+    return 10;
+}
+
+// 함수의 타입을 정의하는 방식
+function sum(a: number, b: number): number {
+    return a + b;
+}
+// sum(10, 20, 30, 40);
+
+// 함수의 옵셔널 파라미터 (선택적 파라미터)   => ? 물음표로 선언한다 !!!
+function log(a: string, b?: string, c?: string) {
+
+}
+log('hello world')
+log('hello ts', 'abc')
