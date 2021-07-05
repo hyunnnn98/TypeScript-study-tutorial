96d7fd1b20e8f1df43dc81caa4ae4f7c774b3e72
SeungHyun Cho <c0305sh@naver.com>
2021-07-05 09:58:18 +0900

    제네릭 실전 예제 살펴보기 - 제네릭을 이용한 타입 정의

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,33 +1,41 @@
-// 매번 타입에 대한 정의를 interface로 선언하다보면 무수한 interface를 볼 수 있게 된다....
-// 한마디로 비효울적이라는 것!!!!!!
-interface Email {
-  value: string;
+// 🔥 하나의 인터페이스로 제네릭을 이용해서 여러가지 타입을 커버하는 방법.
+interface DropdownItem<T> {
+  value: T;
   selected: boolean;
 }
+// 매번 타입에 대한 정의를 interface로 선언하다보면 무수한 interface를 볼 수 있게 된다....
+// 한마디로 비효울적이라는 것!!!!!!
 
-const emails: Email[] = [
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
+
+const emails: DropdownItem<string>[] = [
   { value: 'naver.com', selected: true },
   { value: 'gmail.com', selected: false },
   { value: 'hanmail.net', selected: false },
 ];
 
-interface ProductNumber {
-  value: number;
-  selected: boolean;
-}
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
 
-interface TrueFalse {
-  value: boolean;
-  selected: boolean;
-}
+// interface TrueFalse {
+//   value: boolean;
+//   selected: boolean;
+// }
 
-const numberOfProducts: ProductNumber[] = [
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item: Email | ProductNumber) {
+// createDropdownItem 함수에 제네릭을 선언하여 유니온 타입을 생략하여
+// 보다 깔끔한 코드를 작성할 수 있다.
+function createDropdownItem<T>(item: DropdownItem<T>) {
   const option = document.createElement('option');
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -37,11 +45,11 @@ function createDropdownItem(item: Email | ProductNumber) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector('#email-dropdown');
   selectTag.appendChild(item);
 });
 
 numberOfProducts.forEach(function (product) {
-  const item = createDropdownItem(product);
+  const item = createDropdownItem<number>(product);
 })
